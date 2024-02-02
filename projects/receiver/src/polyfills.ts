de64abbe3a497cfbabf664fd157fdc5fb288c91e
Florian <fiedlefl@gmail.com>
2024-02-02 13:42:30 +0100

    chore(app): dont import babel-polyfills

--- projects/receiver/src/polyfills.ts
+++ projects/receiver/src/polyfills.ts
@@ -1,13 +1,13 @@
 // import 'babel-polyfill';
 
-// (window as any).global = window;
-// (window as any).process = {
-//   ...(window as any).process,
-//   env: { DEBUG: undefined },
-// };
+(window as any).global = window;
+(window as any).process = {
+  ...(window as any).process,
+  env: { DEBUG: undefined },
+};
 
-// // disable patching requestAnimationFrame
-// (window as any).__Zone_disable_requestAnimationFrame = true;
+// disable patching requestAnimationFrame
+(window as any).__Zone_disable_requestAnimationFrame = true;
 
-// // disable patching specified eventNames
-// (window as any).__zone_symbol__UNPATCHED_EVENTS = ['message'];
+// disable patching specified eventNames
+(window as any).__zone_symbol__UNPATCHED_EVENTS = ['message'];
