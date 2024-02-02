5c04c874a30343e2d68480f0e92e38c4c9837b7d
Florian <fiedlefl@gmail.com>
2024-02-02 12:12:28 +0100

    feat(app): add service worker

--- projects/receiver/src/app/app.module.ts
+++ projects/receiver/src/app/app.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { RouterModule, Routes } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 import { IncomingCallComponent } from './components/incoming-call.component';
 
@@ -21,7 +22,16 @@ const routes: Routes = [
 ];
 @NgModule({
   declarations: [AppComponent, IncomingCallComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
